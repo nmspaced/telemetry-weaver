1822bc2f2ef8d0f1b93c548ec787409df54e40cd
Andrew Orinin <andrew.orinin@tarkov.com>
2026-09-23 11:36:26 +0300

    fix: metrics export

--- tests/Fake/FailingLifecycleMetricExporter.php
+++ tests/Fake/FailingLifecycleMetricExporter.php
@@ -63,7 +63,7 @@ final class FailingLifecycleMetricExporter implements PushMetricExporterInterfac
         throw $this->failure;
     }
 
-    public function temporality(MetricMetadataInterface $metric): Temporality|string|null
+    public function temporality(MetricMetadataInterface $_metric): Temporality|string|null
     {
         return Temporality::DELTA;
     }
